d952cb6045648d425344b908467388f564751ec8
michaelblake161 <47145394+michaelblake161@users.noreply.github.com>
2020-10-08 23:44:25 +1100

    Added class to test route time sorting functionality

---
+++ src/controller/TestRouteTimeSorting.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Polygon;
+
+import com.google.gson.JsonObject;
+
+import model.GST;
+
+public class TestRouteTimeSorting {
+
+	public static GST findClosestGst(Coordinate jobCoord, int timeBudgetInSeconds, LocalDateTime departureTime)
+			throws IOException {
+
+		String path2 = "GSTFiles/gstTestData.csv";
+		GSTFactory.readGSTsFromCSV(path2);
+
+		JsonObject jsonObj = AzureMapsApi.getIsochroneCoords(jobCoord, timeBudgetInSeconds, departureTime);
+		Polygon poly = AzureMapsApi.BuildPolygon(jsonObj);
+		ArrayList<GST> nearbyGSTs = new ArrayList<GST>();
+
+		for (GST g : GSTFactory.getGSTpool()) {
+			Coordinate gstCoord = new Coordinate(g.getLat(), g.getLon());
+			if (AzureMapsApi.checkIfLocationInIsochrone(poly, gstCoord)) {
+				int travelTime = AzureMapsApi.getRouteTime(gstCoord, jobCoord);
+				g.setTravelTime(travelTime);
+				nearbyGSTs.add(g);
+
+			}
+		}
+		
+		System.out.println("GSTs in isochrone " + nearbyGSTs);
+
+		System.out.println("Unsorted");
+		for (GST gst : nearbyGSTs) {
+			System.out.println(gst.getTravelTime() + " " + gst.getgSTid());
+		}
+
+		Collections.sort(nearbyGSTs);
+
+		System.out.println("Sorted");
+		for (GST gst : nearbyGSTs) {
+			System.out.println(gst.getTravelTime() + " " + gst.getgSTid());
+		}
+
+		System.out.println("The Closest GST was " + nearbyGSTs.get(0));
+		return nearbyGSTs.get(0);
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		Coordinate coord = AzureMapsApi.getCoordinatesFromAddress("15", "Sully St", "Randwick", "2031");
+		System.out.println(coord);
+		findClosestGst(coord, 1800, LocalDateTime.now());
+
+	}
+
+}
