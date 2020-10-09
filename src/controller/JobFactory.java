cfb97685cf4e4954c12c911a688b0e47316ac342
michaelblake161 <47145394+michaelblake161@users.noreply.github.com>
2020-10-09 23:27:48 +1100

    remove unused imports

--- src/controller/JobFactory.java
+++ src/controller/JobFactory.java
@@ -8,8 +8,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 import model.Job;
