a1c95f081ae0d7aea9bfc505a68a4b6cc088426b
Sawmyaa <55011204+s3734971-Saw@users.noreply.github.com>
2020-10-13 17:42:50 +1100

    Change year to fututre year

--- src/model/Job.java
+++ src/model/Job.java
@@ -177,7 +177,14 @@ public class Job implements Comparable<Job> {
 	}
 
 	public LocalDateTime getOrderCreateDateAndTime() {
-		return orderCreateDateAndTime;
+
+		if (this.orderCreateDateAndTime.isBefore(LocalDateTime.now().plusDays(1))) {
+			long yearsBehind = orderCreateDateAndTime.getYear();
+			long yearsToAdd = LocalDateTime.now().getYear() - yearsBehind;
+			yearsToAdd += 1;
+			return orderCreateDateAndTime.plusYears(yearsToAdd);
+		} else
+			return orderCreateDateAndTime;
 	}
 
 	public String getSuburb() {
@@ -267,18 +274,16 @@ public class Job implements Comparable<Job> {
 	public void setIdleTime(long idleTime) {
 		this.idleTime = idleTime;
 	}
-	
+
 	public long getIdleTime() {
 		return idleTime;
 	}
 
 	public long calculateIdleTime(LocalDateTime orderCreateTime, LocalDateTime timeJobAssigned) {
 		long seconds = orderCreateTime.until(timeJobAssigned, ChronoUnit.SECONDS);
-		//System.out.println(seconds);
+		// System.out.println(seconds);
 		return seconds;
 	}
-
-	
 
 	@Override
 	public int compareTo(Job o) {
@@ -290,7 +295,8 @@ public class Job implements Comparable<Job> {
 		return "Job [Number: " + orderNum + ", FitterDistrict: " + fitterDistrict + " Job Priority: " + jobPriority
 				+ ", Activity Type: " + mainActType + ", Starting Date: " + orderCreateDate + ", Starting Time: "
 				+ orderCreateTime + ", Duration: " + jobDurationInMinutes + ", Travel Time: "
-				+ Simulation.formatSeconds(travelTimeInSeconds) +", Idle Time: " +Simulation.formatSeconds(idleTime) + ", End Date And Time: " + endDateAndTime + "]";
+				+ Simulation.formatSeconds(travelTimeInSeconds) + ", Idle Time: " + Simulation.formatSeconds(idleTime)
+				+ ", End Date And Time: " + endDateAndTime + "]";
 	}
 
 }
