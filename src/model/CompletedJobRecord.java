c25e26aa893e03b17a409e2ea0f57b86924689d1
michaelblake161 <47145394+michaelblake161@users.noreply.github.com>
2020-10-16 03:35:05 +1100

    Added opencsv bindings to instance variables

--- src/model/CompletedJobRecord.java
+++ src/model/CompletedJobRecord.java
@@ -1,4 +1,8 @@
 package model;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+
 /**
  * @author Michael Blake
  * 
@@ -6,10 +10,29 @@ package model;
  *         which have been assigned and completed
  */
 public class CompletedJobRecord {
-	
+
 	private GST gst;
 	private Job job;
 	
+	@CsvBindByName(column = "A-Job ID")
+	private String createNum;
+	@CsvBindByName(column = "B-Address")
+	private String address;
+	@CsvBindByName(column = "C-Suburb")
+	private String suburb;
+	@CsvBindByName(column = "D-Postcode")
+	private String postcode;
+	@CsvBindByName(column = "E-Start Date Time")
+	private String startDateAndTime;
+	@CsvBindByName(column = "F-Idle Time mins")
+	private String idleTime;
+	@CsvBindByName(column = "G-Travel Time mins")
+	private Integer travelTime;
+	@CsvBindByName(column = "H-Job Duration mins")
+	private Integer jobDuration;
+	@CsvBindByName(column = "I-End Date Time")
+	private String endDateAndTime;
+
 	/**
 	 * @param gst
 	 * @param job
@@ -17,16 +40,21 @@ public class CompletedJobRecord {
 	public CompletedJobRecord(GST gst, Job job) {
 		this.gst = gst;
 		this.job = job;
+		this.createNum = Integer.toString(job.getOrderNum());
+		this.address = job.getHouseNum1() + " " + job.getStreet();
+		this.suburb = job.getSuburb();
+		this.postcode = job.getPostcode();
+		this.startDateAndTime = job.getOrderCreateDateAndTime().toString();
+		this.idleTime = Long.toString(job.getIdleTime());
+		this.travelTime = (job.getTravelTimeInSeconds() / 60);
+		this.jobDuration = job.getJobDuration();
+		this.endDateAndTime = job.getEndDateAndTime().toString();
+
 	}
 
 	@Override
 	public String toString() {
-		return "CompletedJobRecord: [Assigned "+ gst + "," + job + "]";
+		return "CompletedJobRecord: [Assigned " + gst + "," + job + "]";
 	}
-	
-	
-	
-
-	
 
 }
