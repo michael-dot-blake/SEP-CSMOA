4aaff4b0217c4ed8966f0cf5e19458358a4a6a6c
michaelblake161 <47145394+michaelblake161@users.noreply.github.com>
2020-11-11 21:20:54 +1100

    Removed API key

--- src/controller/AzureMapsApi.java
+++ src/controller/AzureMapsApi.java
@@ -34,7 +34,7 @@ public class AzureMapsApi {
 	private static final String STATE = "NSW";
 
 	// Insert Azure Maps API key here
-	private static final String API_KEY = "5nXsFMSUBlUyt_Hvq0fgM6u6tKXy80wgwWfvZaLJuj0";
+	private static final String API_KEY = "";
 
 	/**
 	 * A method which takes an address and calls the Azure Maps Api to return
